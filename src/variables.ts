85d27e0648a568d512494bb15e2b9f06ad94bb58
bryce <50299051+bryce-seifert@users.noreply.github.com>
2025-09-25 10:31:43 -0700

    fix: remove unused variables

--- src/variables.ts
+++ src/variables.ts
@@ -1,9 +1,5 @@
 import type { TelevicConferoInstance } from './main.js'
 
 export function UpdateVariableDefinitions(self: TelevicConferoInstance): void {
-	self.setVariableDefinitions([
-		{ variableId: 'variable1', name: 'My first variable' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
-	])
+	self.setVariableDefinitions([])
 }
